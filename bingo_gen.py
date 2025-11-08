1f154d1bff17ee02f9fd8e378845fbfa0b40d804
xavier <xavierbertomeuplatas@gmail.com>
2025-11-08 21:45:50 +0100

    Clash Bingo

--- bingo_gen.py
+++ bingo_gen.py
@@ -315,7 +315,7 @@ def generate_bingo_card(
     return bingo, bingo_text
 
 if __name__ == "__main__":
-    st.header("Bingo Clash Royale")
+    st.header("Clash Royale Bingo")
 
     if "bingos" not in st.session_state:
         st.session_state.bingos = [None, None]
