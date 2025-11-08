690a6599d7bd621e0791866d63a1692b70ed4abf
xavier <xavierbertomeuplatas@gmail.com>
2025-11-08 19:28:15 +0100

    Toggle Test

--- bingo_gen.py
+++ bingo_gen.py
@@ -244,7 +244,7 @@ if __name__ == "__main__":
             elixir_rate = st.slider("Elixir weight", 0.01, 5.0, 0.1)
         
 
-    col1, col2, right = st.columns([1, 1, 5])
+    col1, col2, right = st.columns([1, 1, 3])
 
     with col1:
         if st.button("Generate Bingo"):
