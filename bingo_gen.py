f57fe7a2648eaed6f7d2af4b7a4060c664bdea26
xavier <xavierbertomeuplatas@gmail.com>
2025-11-08 23:24:42 +0100

    Clash Bingo

--- bingo_gen.py
+++ bingo_gen.py
@@ -321,8 +321,8 @@ if __name__ == "__main__":
     )    
 
     st.markdown(
-    '<span style="font-size:32px; font-weight:bold;">Clash Royale Bingo</span> '
-    '<span style="color:gray; font-size:14px;">by @pinxevi</span>',
+    '<span style="font-size:40px; font-weight:bold;">Clash Royale Bingo</span> '
+    '<span style="color:gray; font-size:14px;"> by @pinxevi</span>',
     unsafe_allow_html=True
     )
 
