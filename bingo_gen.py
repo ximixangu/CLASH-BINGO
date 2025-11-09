39293bf860f73aecc43bffc0eb9dbdb2aa4f8377
xavier <xavierbertomeuplatas@gmail.com>
2025-11-09 19:43:19 +0100

    Clash Bingo

--- bingo_gen.py
+++ bingo_gen.py
@@ -163,7 +163,10 @@ def create_cell_content(cell_type):
 
         if card:
             load_and_paste(img, CARDS_PATH / card, (180, 180), (8, 0))
-        load_and_paste(img, ASSETS_PATH / "aux/boost.png", (80, 80), (116, 107))
+        load_and_paste(img, ASSETS_PATH / "aux/boost2.png", (75, 75), (114, 115))
+        # if card:
+        #     load_and_paste(img, CARDS_PATH / card, (180, 180), (8, 0))
+        # load_and_paste(img, ASSETS_PATH / "aux/boost.png", (80, 80), (116, 107))
 
     elif cell_type == 'arena':
         img = Image.new('RGBA', (190, 190), (255, 255, 255, 0))
